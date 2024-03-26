169f573cb947c737fabf4f851dbe407d347e9c8f
maxcorte <m.delcorte@students.ephec.be>
2024-03-26 13:20:22 +0100

    page jeu 2

--- fronted/login/src/app/pages/carousel/carousel.component.ts
+++ fronted/login/src/app/pages/carousel/carousel.component.ts
@@ -1,11 +1,14 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, Input} from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [
-  ],
+    imports: [
+        RouterLink,
+        RouterLinkActive
+    ],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
