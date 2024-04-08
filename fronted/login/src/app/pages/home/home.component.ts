9565d685674d3eb9ff79d48aef074c8972200d36
Schuuka <tonytran.thi10@gmail.com>
2024-04-08 11:43:56 +0200

    Ajout d'un menu sur la gauche dépliable par bouton

--- fronted/login/src/app/pages/home/home.component.ts
+++ fronted/login/src/app/pages/home/home.component.ts
@@ -2,8 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 import {ToolbarComponent} from "../toolbar/toolbar.component";
 import {CarouselComponent} from "../carousel/carousel.component";
 import { SearchBarComponent } from '../search-bar/search-bar.component';
-import { SidenavComponent } from '../sidenav/sidenav.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
@@ -13,12 +14,14 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     ToolbarComponent,
     CarouselComponent,
     SearchBarComponent,
-    SidenavComponent,
-    MatSidenavModule
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatIconModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  
+  opened = false;
+  isFlipped = false;
 }
