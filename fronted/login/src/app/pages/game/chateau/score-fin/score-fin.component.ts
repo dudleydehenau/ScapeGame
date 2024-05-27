02548f838817e739207bc7eff955f789c3f329d0
Patrycja Drewnowska <patty@MacBook-Pro-de-patty.local>
2024-05-27 18:21:22 +0200

    amélioration du service Scores

--- fronted/login/src/app/pages/game/chateau/score-fin/score-fin.component.ts
+++ fronted/login/src/app/pages/game/chateau/score-fin/score-fin.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from '../../../../services/auth.service';
+import { ScoresService } from '../../../../services/scores.service';
 
 @Component({
   selector: 'app-score-fin',
@@ -7,11 +9,43 @@ import { Component, Input } from '@angular/core';
   templateUrl: './score-fin.component.html',
   styleUrl: './score-fin.component.scss'
 })
-export class ScoreFinComponent {
+export class ScoreFinComponent implements OnInit {
 
   @Input()
   totalScore!: number;
 
-  
+  levelId = 3;
+  userId = this.authService.userId.toString();
+  bestScore!: number;
+
+  constructor(private scoresService: ScoresService, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.submitScore();
+  }
+
+  submitScore() {
+    this.scoresService.submitScore(this.levelId, this.userId, this.totalScore).subscribe(
+      response => {
+        console.log('Score soumis avec succès :', response);
+        this.getBestScore();
+      },
+      error => {
+        console.error('Erreur lors de la soumission du score :', error);
+      }
+    );
+  }
+
+  getBestScore() {
+    this.scoresService.getBestScore(this.levelId, this.userId).subscribe(
+      bestScore => {
+        this.bestScore = bestScore;
+        console.log('Meilleur score récupéré :', bestScore);
+      },
+      error => {
+        console.error('Erreur lors de la récupération du meilleur score :', error);
+      }
+    );
+  }
 
 }
